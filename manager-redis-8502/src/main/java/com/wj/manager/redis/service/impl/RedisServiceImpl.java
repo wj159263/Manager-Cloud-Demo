cc8891de4fe162af1339f1493ad3c0692d62fa0c
WwjJ <36468715+wj159263@users.noreply.github.com>
2020-03-30 01:09:51 +0800

    Update RedisServiceImpl.java

--- manager-redis-8502/src/main/java/com/wj/manager/redis/service/impl/RedisServiceImpl.java
+++ manager-redis-8502/src/main/java/com/wj/manager/redis/service/impl/RedisServiceImpl.java
@@ -59,4 +59,57 @@ public class RedisServiceImpl implements RedisService {
         List<Object> persion1 = hash.values("persion");
         System.out.println(persion1);
     }
+      @Override
+    /**
+     * redis使用lua脚本
+     * 脚本运行期间出错不会回滚
+     */
+    public Object userScript(){
+        Random random = new Random();
+        byte[] bytes = "gaskdmgfj223".getBytes();
+        int length = bytes.length;
+        byte[] bytes1 = new byte[2];
+        for (int j = 0; j < 2; j++) {
+            bytes1[j] = bytes[random.nextInt(length)];
+        }
+        String name = new String(bytes1);
+        int i = random.nextInt(27);
+        System.out.println("123");
+
+        //cjson.decode
+        //redis使用lua：
+        String scriptText = "local nowval = redis.call('hincrby',KEYS[1],KEYS[2],1);\n" +
+                "local temp1 = ARGV[1];" +
+                "if type(nowval) == 'string' then \n" +
+                "nowval = tonumber(nowval); \n" +
+                "end \n" +
+                "if type(temp1) == 'string' then \n" +
+                "temp1 = tonumber(temp1); \n" +
+                "end \n" +
+                "if nowval > temp1 then \n" +
+                "local user1 = {};" +
+                "user1['name'] = ARGV[2]; \n" +
+                "user1['age'] = ARGV[3]; \n" +
+                "local x = cjson.encode(user1);" +
+                "redis.call('lpush',KEYS[3],x); \n" +
+                "return x;" +
+                "else \n" +
+                "return 0; \n" +
+                "end";
+        DefaultRedisScript<List> redisScript = new DefaultRedisScript<List>();
+        //Long\Boolean\list\DeSerialized之一。写Object的话，返回是默认List
+        redisScript.setResultType(List.class);
+        redisScript.setScriptText(scriptText);
+        List<String> keys = new ArrayList<String>();
+        keys.add("map");
+        keys.add("key1");
+        keys.add("user_list");
+        Object[] arg = {5,name,i};
+        Object execute = redisTemplate.execute(redisScript, keys, arg);
+
+        String typeName = execute.getClass().getTypeName();
+        System.out.println(execute.getClass().getTypeName());
+        System.out.println(execute);
+        return execute;
+    }
 }
